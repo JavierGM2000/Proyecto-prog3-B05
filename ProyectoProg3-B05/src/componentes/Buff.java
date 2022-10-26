4079b2c1a8456123508c1197e57419799911315a
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2022-10-26 08:42:47 +0200

    Constructor con parametros y verificacion de datos

--- ProyectoProg3-B05/src/componentes/Buff.java
+++ ProyectoProg3-B05/src/componentes/Buff.java
@@ -26,14 +26,54 @@ public class Buff {
 	
 	//Constructor con parametros
 	public Buff(double buffProgreso, double nerfProgreso, double buffDinero, double nerfDinero, double buffFelicidad, double nerfFelicidad, double buffEstudio, double nerfEstudio, int modDias) {
-		this.buffProgreso = buffProgreso;
-		this.nerfProgreso = nerfProgreso;
-		this.buffDinero = buffDinero;
-		this.nerfDinero = nerfDinero;
-		this.buffFelicidad = buffFelicidad;
-		this.nerfFelicidad = nerfFelicidad;
-		this.buffEstudio = buffEstudio;
-		this.nerfEstudio = nerfEstudio;
+		if (buffProgreso < -1) {
+			this.buffProgreso = -1;
+		}else {
+			this.buffProgreso = buffProgreso;
+		}
+		
+		if (nerfProgreso < -1) {
+			this.nerfProgreso = -1;
+		}else {
+			this.nerfProgreso = nerfProgreso;
+		}
+		
+		if (buffDinero < -1) {
+			this.buffDinero = -1;
+		}else {
+			this.buffDinero = buffDinero;
+		}
+		
+		if (nerfDinero < -1) {
+			this.nerfDinero = -1;
+		}else {
+			this.nerfDinero = nerfDinero;
+		}
+		
+		if (buffFelicidad < -1) {
+			this.buffFelicidad = -1;
+		}else {
+			this.buffFelicidad = buffFelicidad;
+		}
+		
+		if (nerfFelicidad < -1) {
+			this.nerfFelicidad = -1;
+		}else {
+			this.nerfFelicidad = nerfFelicidad;
+		}
+		
+		if (buffEstudio < -1) {
+			this.buffEstudio = -1;
+		}else {
+			this.buffEstudio = buffEstudio;
+		}
+		
+		if (nerfEstudio < -1) {
+			this.nerfEstudio = -1;
+		}else {
+			this.nerfEstudio = nerfEstudio;
+		}
+
 		this.modDias = modDias;
 	}
 }
